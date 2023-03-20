c8ed4f95859c55389f970ff416cc580a435f8a4c
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-03-20 22:12:06 +0100

    Update index.jsx
    menu ready

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,28 +1,35 @@
 import React from 'react'
+import home from '../../assets/icones/home-ativo.png'
+import mostLiked from '../../assets/icones/mais-curtidas-inativo.png'
+import mostViewed from '../../assets/icones/mais-vistas-inativo.png'
+import news from '../../assets/icones/novas-inativo.png'
+import surpriseMe from '../../assets/icones/surpreenda-me-inativo.png'
+import styles from './Menu.module.scss'
+
 
 export default function Menu() {
   return (
-    <nav>
-        <ul>    
-            <li>
-                <img src="" alt="" />
-                <a href="">Start</a>
+    <nav className={styles.menu}>
+        <ul className={styles.menu__lista}>    
+            <li className={styles.menu__item}>
+                <img src={home} alt="" />
+                <a href="">Home</a>
             </li>
-            <li>
-                <img src="" alt="" />
+            <li className={styles.menu__item}>
+                <img src={mostLiked} alt="" />
                 <a href="">Most liked</a>
             </li>
-            <li>
-                <img src="" alt="" />
+            <li className={styles.menu__item}>
+                <img src={mostViewed} alt="" />
                 <a href="">Most Viewed</a>
             </li>
-            <li>
-                <img src="" alt="" />
+            <li className={styles.menu__item}>
+                <img src={news} alt="" />
                 <a href="">News</a>
             </li>
-            <li>
-                <img src="" alt="" />
-                <a href="">Get surprised</a>
+            <li className={styles.menu__item}>
+                <img src={surpriseMe} alt="" />
+                <a href="">Surprise me</a>
             </li>
         </ul>
     </nav>
