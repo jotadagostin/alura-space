cdd6008b370ec98016e84b34c90be4ef99810533
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-03-20 21:58:50 +0100

    starting menu

--- src/Pages/FirstPage/index.jsx
+++ src/Pages/FirstPage/index.jsx
@@ -1,7 +1,11 @@
 import Header from "../../components/Header";
+import Menu from "../../components/Menu";
 
 export default function FirstPage() {
   return (
-    <Header/>
-  )
+    <>
+      <Header />
+      <Menu />
+    </>
+  );
 }
---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <nav>
+        <ul>    
+            <li>
+                <img src="" alt="" />
+                <a href="">Start</a>
+            </li>
+            <li>
+                <img src="" alt="" />
+                <a href="">Most liked</a>
+            </li>
+            <li>
+                <img src="" alt="" />
+                <a href="">Most Viewed</a>
+            </li>
+            <li>
+                <img src="" alt="" />
+                <a href="">News</a>
+            </li>
+            <li>
+                <img src="" alt="" />
+                <a href="">Get surprised</a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
