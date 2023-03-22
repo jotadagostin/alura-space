bac67b1b7482d096a3ec8af6a777a162a699bb19
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-03-22 22:51:16 +0100

    cards
    fixiing some bugs

--- src/components/Gallery/Cards/index.jsx
+++ src/components/Gallery/Cards/index.jsx
@@ -1,33 +1,29 @@
 import React from "react";
-import open from "./open.png"
-import favorito from "./favorito.png"
+import open from './open.png'
+import like from './favorito.png'
 
 export default function Cards({itens, styles}) {
   return (
-    <div>
-      return (
-      <ul className={styles.gallery__cards}>
-        {itens.map((item) => {
-          return (
-            <li key={item.id} className={styles.gallery__card}>
-              <img
-                className={styles.gallery__image}
-                src={item.image}
-                alt={item.title}
-              />
-              <p className={styles.gallery__description}>{item.title}</p>
-              <div>
-                <p>{item.credit}</p>
-                <span>
-                  <img src={favorito} alt="heart icon like" />
-                  <img src={open} alt="icon open modal" />
-                </span>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-      )
-    </div>
+    <ul className={styles.gallery__cards}>
+      {itens.map((item) => {
+        return (
+          <li key={item.id} className={styles.gallery__card}>
+            <img
+              className={styles.gallery__image}
+              src={item.image}
+              alt={item.title}
+            />
+            <p className={styles.gallery__description}>{item.title}</p>
+            <div>
+              <p>{item.credit}</p>
+              <span>
+                <img src={like} alt="icon like heart" />
+                <img src={open} alt="icon open modal" />
+              </span>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import Tags from "../Tags";
-import photos from "./photos.json";
 import styles from "./Gallery.module.scss";
-
+import photos from "./photos.json";
 import Cards from "./Cards";
 
 export default function Gallery() {
   return (
     <section className={styles.gallery}>
-      <h2>Browse through the Gallery </h2>
+      <h2>Browse through the Gallery</h2>
       <Tags />
-      <Cards itens={photos} styles={styles}/>
+      <Cards itens={photos} styles={styles} />
     </section>
   );
 }
