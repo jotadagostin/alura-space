5142b47e491d62b09635660fbd72bfeaacc0a115
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-03-22 11:51:53 +0100

    cards
    components cards

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -2,35 +2,15 @@ import React from "react";
 import Tags from "../Tags";
 import photos from "./photos.json";
 import styles from "./Gallery.module.scss";
-import open from "./open.png"
-import favorito from "./favorito.png"
+
+import Cards from "./Cards";
 
 export default function Gallery() {
   return (
     <section className={styles.gallery}>
       <h2>Browse through the Gallery </h2>
       <Tags />
-      <ul className={styles.gallery__cards}>
-        {photos.map((photo) => {
-          return (
-            <li key={photo.id} className={styles.gallery__card}>
-              <img
-                className={styles.gallery__image}
-                src={photo.image}
-                alt={photo.title}
-              />
-              <p className={styles.gallery__description}>{photo.title}</p>
-              <div>
-                <p>{photo.credit}</p>
-                <span>
-                  <img src={favorito} alt="heart icon like" />
-                  <img src={open} alt="icon open modal" />
-                </span>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+      <Cards itens={photos} styles={styles}/>
     </section>
   );
 }
